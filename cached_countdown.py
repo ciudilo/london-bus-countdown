fd9213070325b7183e16180edf515690f54b943f
ajermakovics <andrejs.jermakovics@gmail.com>
2011-09-22 23:21:59 +0100

    Added caching of results
    Caches json response for 60 seconds

---
+++ cached_countdown.py
@@ -0,0 +1,23 @@
+from parse_countdown import Countdown
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+cache_opts = {
+    'cache.type': 'memory' # or 'file'
+}
+countdown_cache = CacheManager(**parse_cache_config_options(cache_opts))
+
+class CachedCountdown(Countdown):
+
+	def __init__(self):
+		print "init cached countdown"
+
+	@countdown_cache.cache('getCountdownJSON', expire=60)
+	def getCountdownJSON(self, *stop_number):
+		json_data = Countdown.getCountdownJSON(self, *stop_number)
+		return json_data
+
+if __name__ == '__main__':
+	countdown = CachedCountdown()
+	print countdown.getCountdownJSON(50980)
+
